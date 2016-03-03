93ca5a220e7d34ea23f65cdf1d25bb0a3c0f33f1
Ryan Madden <ryanmadden2017@u.northwestern.edu>
2016-03-03 14:52:25 -0600

    Update gulpfile and create src and dist dirs

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,20 +2,24 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var postcss      = require('gulp-postcss');
-var sourcemaps   = require('gulp-sourcemaps');
+var cssnano = require('gulp-cssnano');
+var sourcemaps = require('gulp-sourcemaps');
 var autoprefixer = require('gulp-autoprefixer');
 
-gulp.task('sass', function () {
-  gulp.src('./sass/**/*.scss')
-  .pipe(sourcemaps.init())
-  .pipe(postcss([ autoprefixer({ browsers: ['last 2 versions'] }) ]))
-  .pipe(sourcemaps.write('.'))
-  .pipe(gulp.dest('./css'));
+gulp.task('workflow', function () {
+  gulp.src('./src/sass/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(cssnano())
+    .pipe(sourcemaps.write('./'))
+
+  .pipe(gulp.dest('./dist/css/'))
 });
 
-gulp.task('sass:watch', function () {
-  gulp.watch('./sass/**/*.scss', ['sass']);
+gulp.task('default', function () {
+  gulp.watch('./src/sass/**/*.scss', ['workflow']);
 });
-
-gulp.task('default',['sass:watch']);
