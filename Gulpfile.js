992837e6d09c853dd5d672621e1c5590a537d7b8
Ryan Madden <ryanmadden2017@u.northwestern.edu>
2016-03-03 12:01:00 -0600

    Add autoprefixer to grunt for browser compatibility

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,14 +1,19 @@
 'use strict';
- 
+
 var gulp = require('gulp');
 var sass = require('gulp-sass');
- 
+var postcss      = require('gulp-postcss');
+var sourcemaps   = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
+
 gulp.task('sass', function () {
   gulp.src('./sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
+  .pipe(sourcemaps.init())
+  .pipe(postcss([ autoprefixer({ browsers: ['last 2 versions'] }) ]))
+  .pipe(sourcemaps.write('.'))
+  .pipe(gulp.dest('./css'));
 });
- 
+
 gulp.task('sass:watch', function () {
   gulp.watch('./sass/**/*.scss', ['sass']);
 });
