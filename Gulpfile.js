863f428ad4d3c01e8d510661b4b0defd8e3d9f05
Ryan Madden <ryanmadden2017@u.northwestern.edu>
2016-03-05 17:20:27 -0600

    Add prod build to gulpfile

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,25 +1,56 @@
 'use strict';
-
+// Vars
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var cssnano = require('gulp-cssnano');
 var sourcemaps = require('gulp-sourcemaps');
 var autoprefixer = require('gulp-autoprefixer');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
 
-gulp.task('workflow', function () {
+// Sass Development
+gulp.task('sass', function () {
   gulp.src('./src/sass/**/*.scss')
-    .pipe(sourcemaps.init())
+  .pipe(sourcemaps.init())
+  .pipe(sass().on('error', sass.logError))
+  .pipe(autoprefixer({
+    browsers: ['last 2 versions'],
+    cascade: false
+  }))
+  .pipe(sourcemaps.write('./'))
+  .pipe(gulp.dest('./test/'))
+});
+
+// JS Development
+gulp.task('scripts', function() {
+  gulp.src('./src/js/*.js')
+  .pipe(concat('rainier.js'))
+  .pipe(uglify())
+  .pipe(gulp.dest('./test/'));
+});
+
+// Dev Watch Task
+gulp.task('default', function () {
+  gulp.watch('./src/sass/**/*.scss', ['sass']);
+  gulp.watch('./src/js/**/*.js', ['scripts']);
+});
+
+// Build for Production
+gulp.task('build', function () {
+  gulp.src('./src/sass/**/*.scss')
+    // .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer({
       browsers: ['last 2 versions'],
       cascade: false
     }))
     .pipe(cssnano())
+    .pipe(concat('rainier.min.css'))
     .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./dist/'));
 
-  .pipe(gulp.dest('./dist/css/'))
-});
-
-gulp.task('default', function () {
-  gulp.watch('./src/sass/**/*.scss', ['workflow']);
-});
+    gulp.src('./src/js/*.js')
+    .pipe(concat('rainier.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/'));
+  });
